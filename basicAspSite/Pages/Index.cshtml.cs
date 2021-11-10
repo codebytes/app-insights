239121754f82fb38247b7fcf871787f49871ea1f
Chris Ayers <clayers@gmail.com>
2021-11-10 12:09:25 -0500

    fix namespaces

--- basicAspSite/Pages/Index.cshtml.cs
+++ basicAspSite/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace aspAppInsights.Pages
+namespace basicAspSite.Pages
 {
     public class IndexModel : PageModel
     {
