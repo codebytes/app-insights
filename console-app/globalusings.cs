4774d1ecb77da4836b7828e1831c8060c07138c1
Chris Ayers <clayers@gmail.com>
2022-03-04 10:04:31 -0500

    Added Console App

---
+++ console-app/globalusings.cs
@@ -0,0 +1,8 @@
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.ApplicationInsights;
+global using Microsoft.ApplicationInsights.Extensibility;
+global using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+global using Microsoft.ApplicationInsights.DataContracts;
+global using Microsoft.ApplicationInsights.DependencyCollector;
+global using System.Net.Http;
+global using System.Threading.Tasks;
